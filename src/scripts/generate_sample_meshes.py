d2bcc2e9aea1bfeea90b3b995470714b94ec95ef
Strong Bad <sbad@homestarrunner.com>
2024-02-01 17:26:16 -0700

    quat stuf

---
+++ src/scripts/generate_sample_meshes.py
@@ -0,0 +1,13 @@
+from src.utils import generate_sphere_oblate_torus_dumbbell
+
+# import os
+#
+# a = os.system("ls")
+# print("generating course meshes")
+# generate_sphere_oblate_torus_dumbbell(
+#     reg=False, coarse=True, fine=False, ultrafine=False
+# )
+# print("generating fine meshes")
+# generate_sphere_oblate_torus_dumbbell(
+#     reg=False, coarse=True, fine=True, ultrafine=False
+# )
